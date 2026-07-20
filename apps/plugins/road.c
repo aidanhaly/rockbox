af182b3110ad0e146aea33af89cfa734edd49048
aidanHaly <aidan.haly@gmail.com>
2026-07-19 23:07:55 -0400

    further efforts to try and render road

--- apps/plugins/road.c
+++ apps/plugins/road.c
@@ -12,8 +12,14 @@ enum plugin_status plugin_start(const void* parameter){
 	rb->lcd_set_foreground(LCD_RGBPACK(3,252,11));
 	rb->lcd_fillrect(0,(LCD_HEIGHT/10)*5.5,LCD_WIDTH,(LCD_HEIGHT/10)*4.5);
         rb->lcd_set_foreground(LCD_RGBPACK(0,0,0));
-	for(int i=1; i<((LCD_HEIGHT/10)*5.5);i++){
-	    rb->lcd_fillrect(i,LCD_HEIGHT-i,LCD_WIDTH-(i*2),1);
+	int distance = 1;
+	for(int i=1; i<((LCD_HEIGHT/10)*4.5);i++){
+	    if(i%3 == 0){
+		distance++;
+	    }
+	    int strip_width = 25*5/distance;
+	    int x = (LCD_WIDTH/2) - (strip_width/2);
+	    rb->lcd_fillrect(x,LCD_HEIGHT-i,strip_width,1);
 	}
 	rb->lcd_update();
 
